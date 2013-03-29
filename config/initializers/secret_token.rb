d8fef8ef1d27e99761dab6b6a4ffcad177e4fdc3
Market <magiess@volny.cz>
2013-03-29 16:21:06 +0100

    Created a rails application and status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prvni::Application.config.secret_token = 'f3afaaae77ecd1194138810d18f5569f138612184c5af0d20d585bc8d6a8bcd6d0d9bf713eb2cdcdcbcd2531bc15b6211e252eade916abddd64bc54d2d8cf7a2'
